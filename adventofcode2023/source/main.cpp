7005d38117944717d64f7e0c39140b478170fc28
Jens Richter <mail@jensrichter.info>
2023-12-02 00:43:04 +0100

    Optimize logging

--- adventofcode2023/source/main.cpp
+++ adventofcode2023/source/main.cpp
@@ -15,25 +15,21 @@ int main(int argc, char* argv[])
 	string resultPart1 = ExecutePart1();
 	const auto endPart1 = high_resolution_clock::now();
 
+	Log("------------------------------");
+
 	const auto startPart2 = high_resolution_clock::now();
 	string resultPart2 = ExecutePart2();
 	const auto endPart2 = high_resolution_clock::now();
 
 	Log("vvvvvvvvvvvvvvvvvvvvvvvvvvvvvv");
 
-	Log("Part1:");
-	Log(resultPart1);
-
-	Log("\nPart2:");
-	Log(resultPart2);
-
-	Log("");
+	LogFormat("\nPart1:\n{}\n\nPart2:\n{}\n\n", resultPart1, resultPart2);
 
 	using DurationT = duration<float, std::milli>;
 	const DurationT::rep durationPart1 = duration_cast<DurationT>(endPart1 - startPart1).count();
 	const DurationT::rep durationPart2 = duration_cast<DurationT>(endPart2 - startPart2).count();
 
-	LogFormat("Part1 {} ms, Part2 {} ms", durationPart1, durationPart2);
+	LogFormat("Part1: {} ms\nPart2: {} ms", durationPart1, durationPart2);
 
 	Log("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
 	return 0;

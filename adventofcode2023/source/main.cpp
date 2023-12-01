f58fb21d2bcfdb8617be524720cef79921369d60
Jens Richter <mail@jensrichter.info>
2023-12-01 04:22:25 +0100

    Profile execution
    - adds LogFormat

--- adventofcode2023/source/main.cpp
+++ adventofcode2023/source/main.cpp
@@ -2,11 +2,23 @@ import common;
 
 import day1;
 
+import <chrono>;
+
 int main(int argc, char* argv[])
 {
 	using namespace aoc;
+	using namespace std::chrono;
+
+	const auto start = high_resolution_clock::now();
 
 	string result = ExecuteDay();
+
+	const auto end = high_resolution_clock::now();
+
+	using DurationT = duration<float, std::milli>;
+	const DurationT::rep duration = duration_cast<DurationT>(end - start).count();
+
+	LogFormat("Finished in {} ms. Result:", duration);
 	Log(result);
 
 	return 0;

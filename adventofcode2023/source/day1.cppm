4429fae79dd3e055eaf0645d8fe627f5d57a15d7
Jens Richter <mail@jensrichter.info>
2023-12-01 02:01:51 +0100

    Add basic structure
    - One folder with project and pipeline per year
    - Single solution with all years projects
    - Select year via startup project
    - Select day via main.cpp import
    - TODO: add include folder (or similar) for incoming 2023 files

---
+++ adventofcode2023/source/day1.cppm
@@ -0,0 +1,11 @@
+export module day1;
+
+import common;
+
+export namespace aoc
+{
+	export string ExecuteDay()
+	{
+		return "";
+	}
+}
---
+++ adventofcode2023/source/main.cpp
@@ -0,0 +1,14 @@
+import common;
+
+import day1;
+
+#pragma warning(push, 0)
+//#include <Windows.h>
+#pragma warning(pop)
+
+int main(int argc, char* argv[])
+{
+	string result = aoc::ExecuteDay();
+	//OutputDebugString(result.c_str());
+	return 0;
+}
